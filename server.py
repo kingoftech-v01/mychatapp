33351442a2536caf740b821fe362a3fed5dde297
Stephane  Victor <victorstephanearthur@gmail.com>
2025-01-03 21:28:13 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 # Configuration du serveur
-HOST = 'https://kingoftechv01.pythonanywhere.com/'  # Adresse IP du serveur
+HOST = ''  # Adresse IP du serveur
 PORT = 55555        # Port d'écoute
 
 # Création du socket serveur
